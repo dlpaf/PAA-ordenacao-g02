222bb52c06bd08f273b4c6e2a7b30e7e0294a8ca
jordanabfranca <jordanafranca@ufpi.edu.br>
2026-05-20 19:44:15 -0300

    hibrido2

--- algoritmos/hibrido2.py
+++ algoritmos/hibrido2.py
@@ -1,49 +1,55 @@
+comparacoes = 0
+
 def insertion_sort_sub(A, p, r):
     """
     Insertion Sort que ordena apenas o subvetor de p até r (inclusive).
     """
+    comp_insertion = 0
     for j in range(p + 1, r + 1):
         key = A[j]
         i = j - 1
         # Move os elementos de A[p..j-1] que são maiores que a key
         # para uma posição à frente da sua posição atual
         while i >= p and A[i] > key:
+            comp_insertion += 1
             A[i + 1] = A[i]
             i = i - 1
         A[i + 1] = key
+    return comp_insertion
 
 def partition(A, p, r):
-    """
-    Particionamento clássico do livro do Cormen (pivô é o último elemento).
-    """
-    x = A[r]  # Pivô
+    # O partition retorna dois valores: a posição do pivô (q) e as comparações feitas (comp)
+    comp_partition = 0
+    x = A[r]
     i = p - 1
     for j in range(p, r):
+        comp_partition += 1
         if A[j] <= x:
             i = i + 1
             A[i], A[j] = A[j], A[i]
     A[i + 1], A[r] = A[r], A[i + 1]
-    return i + 1
+    
+    return i + 1, comp_partition
 
 def quicksort_hibrido(A, p, r):
-    """
-    Quick Sort Híbrido com Otimização de Recursão de Cauda 
-    e corte para Insertion Sort se o tamanho for menor que k (16).
-    """
+    total_comparacoes = 0
+    
     while p < r:
-        # Se o tamanho do subvetor for menor que 16 (k = 16)
         if (r - p + 1) < 16:
-            insertion_sort_sub(A, p, r)
+            # Soma as comparações do Insertion Sort e encerra o laço
+            total_comparacoes += insertion_sort_sub(A, p, r)
             break
         else:
-            # Particiona o vetor
-            q = partition(A, p, r)
+            # Recebe o índice do pivô e as comparações feitas no particionamento
+            q, comp_p = partition(A, p, r)
+            total_comparacoes += comp_p
             
-            # Otimização da pilha: faz a recursão na MENOR partição primeiro
-            # e atualiza os limites do laço while para a MAIOR partição.
+            # Otimização da pilha de recursão somando os retornos das chamadas
             if (q - p) < (r - q):
-                quicksort_hibrido(A, p, q - 1) # Menor partição à esquerda
-                p = q + 1                      # Transforma a recursão da direita em iteração
+                total_comparacoes += quicksort_hibrido(A, p, q - 1)  # Soma chamadas recursivas
+                p = q + 1
             else:
-                quicksort_hibrido(A, q + 1, r) # Menor partição à direita
-                r = q - 1                      # Transforma a recursão da esquerda em iteração
+                total_comparacoes += quicksort_hibrido(A, q + 1, r)  # Soma chamadas recursivas
+                r = q - 1
+                
+    return total_comparacoes  # Retorna o total acumulado para a main
